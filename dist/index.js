d361d1a2f6508996a1e2807a3118b4fc2ee194d3
sophiehu2509 <sophie.huye@gmail.com>
2021-02-24 21:46:58 -0500

    typeScript

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+// let say:string = "hello";
+let u;
+u = {
+    name: "890",
+    age: 34,
+    gender: "男"
+};
+function getUsers(g) {
+    return [];
+}
---
+++ src/index.ts
@@ -0,0 +1,37 @@
+// let say:string = "hello";
+
+// function sum(a:number, b:number): number { 
+//     return a + b;
+// }
+// sum (3,4)
+// console.log("324256")
+
+// let nums: number []; 
+// // 数组，每一项是数字
+
+// let num: Array<number> = [3, 4, 5]
+
+// let u:object;
+// u = {
+//     name:'sdf',
+//     age:343
+// }
+type Gender = "男" | "女"
+
+type User = {
+    name: string
+    age: number
+    gender: "男" | "女"
+}
+
+let u: User
+
+u = {
+    name: "890",
+    age: 34,
+    gender: "男"
+}
+
+function getUsers(g:Gender): User[] {
+    return [];
+}
